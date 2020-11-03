648e70f60b3f1afadc6184e80df34d7a51d5fe49
Duncanista <30836115+duncanista@users.noreply.github.com>
2020-11-03 15:39:51 -0600

    Add basic structure of the api

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,21 @@
 const functions = require('firebase-functions');
+const express = require('express');
+const bodyParser = require('body-parser');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extendend: true }));
+
+// const api = require('./api');
+// app.use('/api', api);
+
+app.get('/hello', (req, res, next) => {
+  res.status(400).json({ message: 'Hello World from Firebase Cloud Functions!'});
+  return;
+})
+
+app.listen(port, () => console.log(`[server] Listening on port ${port}`));
+
+exports.app = functions.https.onRequest(app);
