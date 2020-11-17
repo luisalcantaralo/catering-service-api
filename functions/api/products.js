e34001c8fb730b22599b6eb0eb5fcaf68e04243f
luisalcantaralo <alcantaraluisalfonso@gmail.com>
2020-11-17 13:22:15 -0600

    testing and minor fixes  in products.js

--- functions/api/products.js
+++ functions/api/products.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const {connectionData} = require('../config/db');
 const { Client } = require('pg');
 
-
+// Done testing
 router.get('/', async(req, res, next) => {
     const client = new Client(connectionData)
     client.connect();
@@ -21,6 +21,26 @@ router.get('/', async(req, res, next) => {
 });
 
 
+// Done testing
+router.get('/:id',async(req, res, next) => {
+    const {id} = req.params;
+    console.log(req.params);
+    const client = new Client(connectionData)
+    client.connect();
+    try {
+        const paramsQuery = [id]; 
+        const data = await client.query('SELECT * FROM products WHERE product_id = $1', paramsQuery);
+        console.log(data.rows) ;
+        res.status(200).json({ message: data.rows});
+
+    } catch (error) {
+        next({status: 500, message: error.stack});
+    } finally{
+        client.end();
+    }
+});
+
+// Done testing
 router.post('/', async(req, res, next) => {
     const client = new Client(connectionData)
     client.connect();
@@ -39,13 +59,14 @@ router.post('/', async(req, res, next) => {
     }
 });
 
+// Done testing
 router.put('/:id', async(req, res, next) => {
     const client = new Client(connectionData)
     client.connect();
     const {id} = req.params;
     const {product_name, description, category, price, measure} = req.body;
     try {
-        const data = await client.query('UPDATE products SET product_name = $2, description = $3, category = $4, price = $5, measure = $6 WHERE customer_id = $1', [id, product_name, description, category, price, measure]);
+        const data = await client.query('UPDATE products SET product_name = $2, description = $3, category = $4, price = $5, measure = $6 WHERE product_id = $1', [id, product_name, description, category, price, measure]);
         console.log(data.rows) ;
         res.status(200).json({ data: data.rows, message: "Successful updating product"});
 
@@ -57,12 +78,12 @@ router.put('/:id', async(req, res, next) => {
     }
 });
 
+// Done testing
 router.delete('/:id', async(req, res, next) => {
     const client = new Client(connectionData)
     client.connect();
     const {id} = req.params;
     try {
-        const paramsQuery = [customer_id]; 
         const data = await client.query(`DELETE FROM products WHERE product_id = ${id}`);
         console.log(data.rows) ;
         res.status(200).json({ message: "Successful deleting item", data: data.rows});
