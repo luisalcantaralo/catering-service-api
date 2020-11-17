12d62c39196fe2ae64c82bb26f8fe7f07eef147d
Joaquin Rios <joaquin.riosco@gmail.com>
2020-11-17 11:28:59 -0600

    Added product to api

---
+++ functions/api/products.js
@@ -0,0 +1,81 @@
+const express = require('express');
+const router = express.Router();
+const {connectionData} = require('../config/db');
+const { Client } = require('pg');
+
+
+router.get('/', async(req, res, next) => {
+    const client = new Client(connectionData)
+    client.connect();
+    
+    try {
+        const data = await client.query('SELECT * FROM products');
+        res.status(200).json({ message: data.rows});
+
+    } catch (error) {
+        next({status: 500, message: error.stack});
+    } finally{
+        client.end();
+    }
+   
+});
+
+
+router.post('/', async(req, res, next) => {
+    const client = new Client(connectionData)
+    client.connect();
+    const { product_name, description, category, price, measure} = req.body;
+    try {
+        var data = await client.query('INSERT INTO products (product_name, description, category, price, measure, active) VALUES($1, $2, $3, $4, $5, true) RETURNING *', [product_name, description, category, price, measure]);
+
+        console.log(data.rows);
+        res.status(200).json({ data: data.rows, message: "Successful inserting product"});
+
+    } catch (error) {
+        res.status(400).json({ message: "Error with query", error: error});
+        
+    } finally{
+        client.end();
+    }
+});
+
+router.put('/:id', async(req, res, next) => {
+    const client = new Client(connectionData)
+    client.connect();
+    const {id} = req.params;
+    const {product_name, description, category, price, measure} = req.body;
+    try {
+        const data = await client.query('UPDATE products SET product_name = $2, description = $3, category = $4, price = $5, measure = $6 WHERE customer_id = $1', [id, product_name, description, category, price, measure]);
+        console.log(data.rows) ;
+        res.status(200).json({ data: data.rows, message: "Successful updating product"});
+
+    } catch (error) {
+        res.status(400).json({ message: "Error with query", error: error});
+        
+    } finally{
+        client.end();
+    }
+});
+
+router.delete('/:id', async(req, res, next) => {
+    const client = new Client(connectionData)
+    client.connect();
+    const {id} = req.params;
+    try {
+        const paramsQuery = [customer_id]; 
+        const data = await client.query(`DELETE FROM products WHERE product_id = ${id}`);
+        console.log(data.rows) ;
+        res.status(200).json({ message: "Successful deleting item", data: data.rows});
+
+    } catch (error) {
+        console.log(error);
+        res.status(400).json({ message: "Error with query", error: error});
+        
+    } finally{
+        client.end();
+    }
+});
+
+
+
+module.exports = router;
