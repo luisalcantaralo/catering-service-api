872963cc79912165e00220be15ff419fbc8db3ab
Duncanista <30836115+duncanista@users.noreply.github.com>
2020-11-16 20:13:40 -0600

    Delete users.js and remove something from the index

--- functions/config/db.js
+++ functions/config/db.js
@@ -1,13 +1,11 @@
 const { Client } = require('pg');
 
 const connectionData = {
-
   user: 'postgres',
   host: 'catering-service.c0zzjro3idpn.us-east-1.rds.amazonaws.com',
   database: 'postgres',
   password: 'KUzrHH16MWY1Qrk9gLda',
   port: 5432,
-
 }
 
 const client = new Client(connectionData)
