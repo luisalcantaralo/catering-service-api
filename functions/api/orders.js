f6e9bdca37af12c90cbfc17412f0f83dd99c7e60
rtperezyera <tllezroberto9@gmail.com>
2020-11-30 22:40:35 -0600

    Implement update functionality on orders

--- functions/api/orders.js
+++ functions/api/orders.js
@@ -139,8 +139,43 @@ router.put('/:id', async(req, res, next) => {
     const client = new Client(connectionData)
     client.connect();
     const {id} = req.params;
-    const {customer_id, order_event, recurring, order_notes, total_price, amount_paid} = req.body;
+    const { order_event, order_notes, recurring,  people, products } = req.body;
     console.log(req.body);
+
+    try {
+
+        let total_price = 0;
+        let queryParameters = [];
+        for (let i=0; i<products.length; ++i){
+
+          let price = await client.query('SELECT price FROM products WHERE product_id = $1', [products[i]["id"]]);
+          price = price.rows[0]["price"];
+
+          total_price += parseFloat(price) * parseFloat(products[i]["quantity"]);
+          queryParameters.push([id, products[i]["id"], products[i]["quantity"], parseFloat(price) * parseFloat(products[i]["quantity"])]);
+        }
+
+        let data = await client.query('UPDATE orders SET order_event = $3, recurring = $4, order_notes= $5, amt_people= $6, total_price= $2 WHERE order_id = $1', [id, total_price, order_event, recurring, order_notes, people]);
+
+        await client.query('DELETE FROM order_product WHERE order_id = $1', [id]);
+
+        let query = pgFormat("INSERT INTO order_product (order_id, product_id, amount, price) VALUES %L", queryParameters);
+        await client.query(query);
+
+        res.status(200).json({ data: data.rows, message: "Successful inserting item"});
+
+    } catch (error) {
+        console.log(error);
+        res.status(400).json({ message: "Error with query", error: error});
+        
+    } finally{
+        client.end();
+    }
+
+
+
+
+
     try {
         const data = await client.query('UPDATE orders SET customer_id = $2, order_event = $3, recurring = $4, order_notes= $5, total_price= $6, amount_paid = $7 WHERE order_id = $1', [id, customer_id, order_event, recurring, order_notes, total_price, amount_paid]);
         console.log(data.rows) ;
@@ -152,6 +187,8 @@ router.put('/:id', async(req, res, next) => {
     } finally{
         client.end();
     }
+    
+
 });
 
 // Deletes order
